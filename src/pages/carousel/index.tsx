bfeda0d8c9975c648dedec157409101404a40b02
paulogermanopg <paulogermano.person@outlook.com.br>
2025-06-02 10:07:47 -0300

    adjustments in components files

--- src/pages/carousel/index.tsx
+++ src/pages/carousel/index.tsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from "react";
 import * as S from "./styles";
 import carouselEmpty from "../../assets/images/carouselEmpty.png";
-import pangare from "../../assets/images/cavalo2.png";
+import pangare from "../../assets/images/cavalo1.png";
+import pangare02 from "../../assets/images/cavalo2.png";
+import pangare03 from "../../assets/images/cavalo3.png";
 
-const images = [pangare, pangare, pangare];
+const images = [pangare, pangare02, pangare03];
 
 export default function Carousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
